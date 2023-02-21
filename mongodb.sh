dbb81c034e5ac9f240b2ddd27a05fcbc4890fc6f
patil164 <patilinfo1994@gmail.com>
2023-02-21 19:29:14 +0530

    print helloword

--- mongodb.sh
+++ mongodb.sh
@@ -3,4 +3,4 @@ yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
 
-#Update /etc/mongodb.conf from 127.0.0.1 to 0.0.0.0
+#Update /etc/mongodb.conf from 127.0.0.1  to 0.0.0.0
