f0060a36284257f905278b99c4385848a6d981aa
patil164 <patilinfo1994@gmail.com>
2023-03-02 21:57:39 +0530

    export

--- frontend.sh
+++ frontend.sh
@@ -2,13 +2,13 @@ code_dir=$(pwd)
 log_file=/tmp/roboshop.log
 rm -f ${log_file}
 
-echo -e "\e[35installing nginx\e[0m"
+echo -e "\e[35 installing nginx\e[0m"
 yum install nginx -y &>>${log_file}
 
-echo -e "\e[35enabling nginx\e[0m"
+echo -e "\e[35 enabling nginx\e[0m"
 systemctl enable nginx &>>${log_file}
 
-echo -e "\e[35starting nginx\e[0m"
+echo -e "\e[35 istarting nginx\e[0m"
 systemctl start nginx &>>${log_file}
 
 echo -e "\e[35removing old content\e[0m"
