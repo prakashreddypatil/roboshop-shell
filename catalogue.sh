3ba87ec98fe2ba48528aab98f192fed825aab50c
patil164 <patilinfo1994@gmail.com>
2023-03-03 18:13:56 +0530

    mongodb

--- catalogue.sh
+++ catalogue.sh
@@ -16,39 +16,39 @@ useradd roboshop  &>>${log_file}
 print_head "create application directory"
 mkdir /app  &>>${log_file}
 
-Print_head "delete old content"
+print_head  "delete old content"
 rm -rf /app/*  &>>${log_file}
 
-Print_head "download the app content"
+print_head  "download the app content"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip  &>>${log_file}
 
 cd /app  &>>${log_file}
 
-print_head "extracting the content"
+print_head  "extracting the content"
 unzip /tmp/catalogue.zip  &>>${log_file}
 
 cd /app  &>>${log_file}
 
-print_head "installing the nodejs dependencies"
+print_head  "installing the nodejs dependencies"
 npm install &>>${log_file}
 
-print_head "copy sysytemd service file"
+print_head  "copy sysytemd service file"
 cp ${code_dir}/configs/catalogue.service  /etc/systemd/system/catalogue.service &>>${log_file}
 
-print_head "reload systemd"
-systemctl daemon-reload &>>${log_file}
+print_head  "reload systemd"
+systemctl daemon-reload  &>>${log_file}
 
-print_head "enable catalogue server"
+print_head  "enable catalogue server"
 systemctl enable catalogue &>>${log_file}
 
-print_head "start catalogue server"
+print_head  "start catalogue server"
 systemctl start catalogue &>>${log_file}
 
-print_head "copy mongodb repo file"
+print_head  "copy mongodb repo file"
 cp ${code_dir}/configs/mongodb.repo  /etc/yum.repos.d/mongodb.repo &>>${log_file}
 
-print_head "install mongodb schema"
+print_head  "install mongodb schema"
 yum install mongodb-org-shell -y &>>${log_file}
 
-print_head "load schema"
-mongo --host mongodb.devops164.online </app/schema/catalogue.js &>>${log_file}
+print_head  "load schema"
+mongo --host mongodb.devops164.online </app/schema/catalogue.js  &>>${log_file}
