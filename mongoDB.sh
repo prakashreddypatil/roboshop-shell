6227563d1a8c8eb09e45f0f287b0dc1da8691277
patil164 <patilinfo1994@gmail.com>
2023-03-03 17:22:10 +0530

    mongodb

--- mongoDB.sh
+++ mongoDB.sh
@@ -1,7 +1,24 @@
-cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org -y
-systemctl enable mongod
-systemctl start mongod
+code_dir=$(pwd)
+log_file=/tmp/roboshop.log
+rm -f ${log_file}
+print_head() {
+  echo -e "\e[35m$1\e[0m"
+}
 
+print_head " copying the repo file"
+cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+
+print_head " installing mongodb"
+yum install mongodb-org -y  &>>${log_file}
+
+print_head " enabling mongodb"
+systemctl enable mongod  &>>${log_file}
+
+print_head " starting momgodb server"
+systemctl start mongod  &>>${log_file}
+
+print_head " updating listin address"
 ## Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongodb.conf
-systemctl restart mongod
+
+print_head " restarting the server "
+systemctl restart mongod  &>>${log_file}
