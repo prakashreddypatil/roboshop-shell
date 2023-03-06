d57f2722d4d20ab239c885c0a117e0f4d8e5ccb4
patil164 <patilinfo1994@gmail.com>
2023-03-06 19:49:06 +0530

    test

---
+++ mongodb.sh
@@ -0,0 +1,25 @@
+source common.sh
+
+print_head " setup the repo file"
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo   &>>${log_file}
+status_check $?
+
+print_head " installing mongodb"
+yum install mongodb-org -y  &>>${log_file}
+status_check $?
+
+print_head " updating listing address"
+sed  -i -e  's/127.0.0.1/0.0.0.0/'  /etc/mongodb.conf   &>>${log_file}
+status_check $?
+## Updated 127.0.0.1 to 0.0.0.0 in /etc/mongodb.conf
+
+print_head " enabling mongodb"
+systemctl enable mongod  &>>${log_file}
+status_check $?
+
+print_head " starting momgodb server"
+systemctl start mongod  &>>${log_file}
+status_check $?
+
+
+
