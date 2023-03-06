681afbd1c5125f0280bf7673d2f813bcaf4aa90f
patil164 <patilinfo1994@gmail.com>
2023-03-06 22:40:26 +0530

    test

--- catalogue.sh
+++ catalogue.sh
@@ -71,5 +71,5 @@ yum install mongodb-org-shell -y  &>>${log_file}
 status_check $?
 
 print_head  "load schema"
-mongo --host  mongodb.devops164.online  </app/schema/catalogue.js   &>>${log_file}
+mongo --host mongodb.devops164.online </app/schema/catalogue.js   &>>${log_file}
 status_check $?
