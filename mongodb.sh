d10384613cc6a9a09e78d37408517da40ee5a141
patil164 <patilinfo1994@gmail.com>
2023-03-08 16:16:14 +0530

    do

--- mongodb.sh
+++ mongodb.sh
@@ -8,11 +8,6 @@ print_head " installing mongodb"
 yum install mongodb-org -y  &>>${log_file}
 status_check $?
 
-print_head " updating listing address"
-sed  -i -e  's/127.0.0.1/0.0.0.0/'  /etc/mongod.conf   &>>${log_file}
-status_check $?
-## Updated 127.0.0.1 to 0.0.0.0 in /etc/mongodb.conf
-
 print_head " enabling mongodb"
 systemctl enable mongod  &>>${log_file}
 status_check $?
@@ -22,4 +17,12 @@ systemctl start mongod  &>>${log_file}
 status_check $?
 
 
+print_head " updating listing address"
+sed  -i -e  's/127.0.0.1/0.0.0.0/'  /etc/mongod.conf   &>>${log_file}
+status_check $?
+## Updated 127.0.0.1 to 0.0.0.0 in /etc/mongodb.conf
+
+print_head " restart momgodb server"
+systemctl restart mongod
+status_check $?
 
